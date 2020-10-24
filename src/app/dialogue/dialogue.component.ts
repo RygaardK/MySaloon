8adc0f3daf37c6ff99b0ec22924c76ba34576a6e
Kristoffer <k.rygaard@gmail.com>
2020-10-24 23:08:06 +0200

    added some inputfields and messages is started up etc.

--- src/app/dialogue/dialogue.component.ts
+++ src/app/dialogue/dialogue.component.ts
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogueComponent implements OnInit {
 
-  public name = "Kristoffer";
+  public isDisabled = true;
+  public isLoggedIn = false;
+  public hasError = false;
+  public isSpecial = false;
+  public name = "Kristoffer R";
+  public messageClasses = {
+    "text-isDisabled": !this.isDisabled,
+    "text-isLoggedIn": !this.isLoggedIn,
+    "text-hasError": !this.hasError,
+    "text-isSpecial": !this.isSpecial,
+  }
   constructor() { }
 
   ngOnInit(): void {
--- src/app/dialogue/user/user.component.ts
+++ src/app/dialogue/user/user.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  
+  public fname = "Kristoffer"
+  public lname = "Rygaard"
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  // function userName(fname,lname) {
+  //   default: "Howdy, stranger. Haven't seen your face around here before. What's your name?"
+  //   return "Alright " + lname + ", what can I do you for?"
+  // }
 }
+  
